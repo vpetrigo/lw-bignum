3d566c67d43045ac32f29e7b827d6a2a5cbc1641
Vladimir Petrigo <taenaru@gmail.com>
2016-04-09 00:38:41 +0300

    add tests cases for String_to_Dec class

--- test/bignum_converter_tests.cpp
+++ test/bignum_converter_tests.cpp
@@ -1,11 +1,14 @@
 #include <gtest/gtest.h>
 #include "bignum_converter.hpp"
 
+/**
+ * Base Converter class's tests
+ */
 TEST(Unsigned_Big_Integer_Converter, pass_zero) {
   constexpr unsigned val = 0;
   const lw_big::BigInt ans{0};
   const auto conv_result = lw_big::Base_Converter::uconvert(val);
-  
+
   EXPECT_EQ(conv_result.size(), ans.size());
   EXPECT_EQ(conv_result[0], ans[0]);
 }
@@ -14,7 +17,7 @@ TEST(Unsigned_Big_Integer_Converter, pass_unsigned_value) {
   constexpr unsigned val = 65486;
   const lw_big::BigInt ans{255, 206};
   const auto conv_result = lw_big::Base_Converter::uconvert(val);
-  
+
   EXPECT_EQ(conv_result.size(), ans.size());
   for (auto i = 0; i < conv_result.size(); ++i) {
     EXPECT_EQ(conv_result[i], ans[i]);
@@ -25,7 +28,7 @@ TEST(Unsigned_Big_Integer_Converter, pass_signed_value) {
   constexpr int val = 65486;
   const lw_big::BigInt ans{255, 206};
   const auto conv_result = lw_big::Base_Converter::uconvert(val);
-  
+
   EXPECT_EQ(conv_result.size(), ans.size());
   for (auto i = 0; i < conv_result.size(); ++i) {
     EXPECT_EQ(conv_result[i], ans[i]);
@@ -36,21 +39,10 @@ TEST(Unsigned_Big_Integer_Converter, pass_negative_signed_value) {
   constexpr auto val = -4611686018427387905;
   const lw_big::BigInt ans{64, 0, 0, 0, 0, 0, 0, 1};
   const auto conv_result = lw_big::Base_Converter::uconvert(val);
-  
+
   EXPECT_EQ(conv_result.size(), ans.size());
   for (auto i = 0; i < conv_result.size(); ++i) {
     EXPECT_EQ(conv_result[i], ans[i]);
-  }
-}
-
-TEST(String_to_Dec_Converter, split_small_string) {
-  const std::string val{"10"};
-  const auto res = lw_big::String_to_Dec::split_string_for_num(val);
-  const lw_big::String_to_Dec::value_type ans{10};
-  
-  EXPECT_EQ(res.size(), ans.size());
-  for (auto i = 0; i < ans.size(); ++i) {
-    EXPECT_EQ(res[i], ans[i]);
   }
 }
 
@@ -58,7 +50,7 @@ TEST(Unsigned_Big_Integer_Converter, pass_unsigned_string) {
   const std::string val{"0"};
   const lw_big::BigInt ans{0};
   const auto conv_result = lw_big::Base_Converter::uconvert(val);
-  
+
   EXPECT_EQ(conv_result.size(), ans.size());
   for (auto i = 0; i < conv_result.size(); ++i) {
     EXPECT_EQ(conv_result[i], ans[i]);
@@ -71,11 +63,54 @@ TEST(Unsigned_Big_Integer_Converter, check_string_and_uint_answer) {
   const lw_big::BigInt ans{127, 255, 255, 255, 255, 255, 255, 255};
   const auto conv_result1 = lw_big::Base_Converter::uconvert(val1);
   const auto conv_result2 = lw_big::Base_Converter::uconvert(val2);
-  
+
   EXPECT_EQ(conv_result1.size(), ans.size());
   EXPECT_EQ(conv_result2.size(), ans.size());
   for (auto i = 0; i < ans.size(); ++i) {
     EXPECT_EQ(conv_result1[i], ans[i]);
     EXPECT_EQ(conv_result2[i], ans[i]);
+  }
+}
+
+/**
+ * String to Dec class's tests
+ */
+TEST(String_to_Dec_Converter, split_small_string) {
+  const std::string val{"10"};
+  const auto res = lw_big::String_to_Dec::split_string_for_num(val);
+  const lw_big::String_to_Dec::value_type ans{10};
+
+  EXPECT_EQ(res.size(), ans.size());
+  for (auto i = 0; i < ans.size(); ++i) {
+    EXPECT_EQ(res[i], ans[i]);
+  }
+}
+
+TEST(String_to_Dec_Converter, split_big_string) {
+  const std::string val(15, '1');
+  const auto res = lw_big::String_to_Dec::split_string_for_num(val);
+  const lw_big::String_to_Dec::value_type ans{1, 1111111, 1111111};
+
+  EXPECT_EQ(res.size(), ans.size());
+  for (auto i = 0; i < ans.size(); ++i) {
+    EXPECT_EQ(res[i], ans[i]);
+  }
+}
+
+TEST(String_to_Dec_Converter, split_very_big_string) {
+  std::string val;
+
+  for (std::size_t i = 0; i < 80; ++i) {
+    val.append("1234567");
+  }
+  val.insert(val.begin(), 2, '9');
+  
+  const auto res = lw_big::String_to_Dec::split_string_for_num(val);
+  lw_big::String_to_Dec::value_type ans(80, 1234567);
+  ans.insert(ans.begin(),99);
+
+  EXPECT_EQ(res.size(), ans.size());
+  for (auto i = 0; i < ans.size(); ++i) {
+    ASSERT_EQ(res[i], ans[i]);
   }
 }

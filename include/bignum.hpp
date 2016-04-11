06c57f91ce6c4321bf441e637a019a36a98478d3
Vladimir Petrigo <taenaru@gmail.com>
2016-04-11 17:40:26 +0300

    add basic Unsigned Big Number implementation

--- include/bignum.hpp
+++ include/bignum.hpp
@@ -2,35 +2,48 @@
 #define BIGNUM_HPP
 
 #include <string>
-#include "bignum_defs.hpp"
 #include "bignum_converter.hpp"
+#include "bignum_defs.hpp"
 
 namespace lw_big {
-  
-  class Bignum {
-    public:
-      virtual std::string repr(base& b) = 0;
-      virtual ~Bignum();
-    protected:
-      Bignum();
-    private:
-      BigInt storage;
-  };
-  
-  class UBignum : public Bignum {
-    public:
-      UBignum();
-      // initialize big number with a string
-      // string might represent a number either in decimal format or binary
-      // example: "12345" - dec; "001001" - bin;
-      UBignum(const std::string& num, const base& b);
-      // initialize big number with a unsigned integer
-      template <typename UnsignedT>
-      UBignum(const UnsignedT num);
-      
-      std::string repr(base& b);
-  };
-  
-} // namespace lw_big
 
-#endif // BIGNUM_HPP
+class Bignum {
+ public:
+  Bignum() = delete;
+  
+  virtual std::string repr(const base& b) const = 0;
+  virtual ~Bignum();
+
+ protected:
+  Bignum(BigInt&& b);
+
+  BigInt storage;
+};
+
+class UBignum : public Bignum {
+ public:
+  // TODO: initialize big number with a string
+  // string might represent a number either in decimal format or binary
+  // example: "12345" - dec; "001001" - bin;
+  explicit UBignum(const std::string& num);
+  // initialize big number with a unsigned integer
+  explicit UBignum(const unsigned long long num);
+  
+  UBignum& operator+=(const UBignum& rhs);
+  const UBignum operator+(const UBignum& rhs) const;
+  UBignum& operator-=(const UBignum& rhs);
+  const UBignum operator-(const UBignum& rhs) const;
+  UBignum& operator*=(const UBignum& rhs);
+  const UBignum operator*(const UBignum& rhs) const;
+  UBignum& operator/=(const UBignum& rhs);
+  const UBignum operator/(const UBignum& rhs) const;
+
+  std::string repr(const base& b) const;
+  
+  private:
+    void add(const BigInt& b);
+};
+
+}  // namespace lw_big
+
+#endif  // BIGNUM_HPP
--- src/bignum.cpp
+++ src/bignum.cpp
@@ -1,11 +1,64 @@
+#include <cstddef>
+#include <algorithm>
 #include "bignum.hpp"
+#include "bignum_defs.hpp"
 
 namespace lw_big {
+Bignum::Bignum(BigInt&& b) : storage{b} {};
+
+Bignum::~Bignum() {}
+
+// Construct unsigned big integer either from string or unsigned integer
+UBignum::UBignum(const std::string& num)
+    : Bignum(Base_Converter::uconvert(num)) {}
+UBignum::UBignum(const unsigned long long num)
+    : Bignum(Base_Converter::uconvert(num)) {}
+
+std::string UBignum::repr(const base& b) const {
+  return Base_Converter::bconvert(storage, b);
+}
+
+UBignum& UBignum::operator+=(const UBignum& rhs) {
+  if (this != &rhs) {
+    this->add(rhs.storage);
+  }
+  
+  return *this;
+}
+
+const UBignum UBignum::operator+(const UBignum& rhs) const {
+  return UBignum(*this) += rhs;
+}
+
+void UBignum::add(const BigInt& b) noexcept {
+  // determine size needed for storing the result
+  auto diff = 0;
+  if (b.size() > storage.size()) {
+    diff = b.size() - storage.size();
+  }
+  BigInt lead_zeros(diff + 1, 0);
+  storage.insert(storage.begin(), lead_zeros.begin(), lead_zeros.end());
+
+  unsigned short remainder = 0;
+  auto a_rev_it = storage.rbegin();
+  auto b_rev_it = b.crbegin();
+  auto b_rev_end = b.crend();
+  
+  while (b_rev_it != b_rev_end) {
+    auto result = *b_rev_it + *a_rev_it + remainder;
     
-    // construct dummy object
-    // UBignum::UBignum() : Bigint() {}
-    
-    //template <typename UnsignedT>
-    // UBignum::UBignum(const UnsignedT num) : storage{Base_Converter::uconvert(num)} {}
-    
-} // namespace lw_big
+    *a_rev_it = result % BINBASE;
+    remainder = result / BINBASE;
+    ++a_rev_it;
+    ++b_rev_it;
+  }
+  
+  if (remainder) {
+    *a_rev_it = remainder;
+  }
+  else {
+    storage.erase(storage.begin());
+  }
+}
+
+}  // namespace lw_big
